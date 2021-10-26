d738b3a11e727fb6307a258e7a8610f434583638
DentonGPaul <dentongpaul@gmail.com>
2021-10-26 15:53:54 -0400

    updated config

--- app/config.py
+++ app/config.py
@@ -4,16 +4,16 @@ class BaseConfig:
     SQLALCHEMY_TRACK_MODIFICATIONS = False
 
 class DevConfig(BaseConfig):
-    mysql_password = "INSERT PASSWORD HERE"
-    SQLALCHEMY_DATABASE_URI = f"mysql://root:{mysql_password}@localhost/nba_dev"
+    SQLALCHEMY_DATABASE_URI = "sqlite:///dev.db"
 
 class TestConfig(BaseConfig):
-    mysql_password = "INSERT PASSWORD HERE"
-    SQLALCHEMY_DATABASE_URI = f"mysql://root:{mysql_password}@localhost/nba_test"
+    SQLALCHEMY_DATABASE_URI = "sqlite:///test.db"
 
 class ProdConfig(BaseConfig):
-    # SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL')
-    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL').replace("://", "ql://", 1)
+    try:
+        SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL').replace("://", "ql://", 1)
+    except:
+        pass
 
 
 configurations = {
