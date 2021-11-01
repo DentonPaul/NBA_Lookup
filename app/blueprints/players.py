0c8cbd62f1b6e20150a832b5a4a7dff479bd2036
DentonGPaul <dentongpaul@gmail.com>
2021-10-31 20:32:53 -0400

    update player page

--- app/blueprints/players.py
+++ app/blueprints/players.py
@@ -1,7 +1,108 @@
 from flask import Blueprint, render_template
+from app.forms import PlayerLookupForm, PlayerAddForm
+import pandas as pd
+from app.extensions import db
+from sqlalchemy import func
+from app.models import Player_Stats
 
 players_bp = Blueprint('players', __name__)
 
 @players_bp.route('/players')
 def index():
     return render_template('players/index.html')
+
+@players_bp.route('/players/search')
+def search():
+    form = PlayerLookupForm()
+    return render_template('players/search.html', form=form)
+
+@players_bp.route('/players/search/create', methods=['GET', 'POST'])
+def create():
+    form = PlayerLookupForm()
+
+    # player simple stats
+    sql_general = f"select pos, age from players where name='{form.PlayerName.data}'"
+    df = pd.read_sql_query(sql_general, db.get_engine())
+    general_rows = [row for index, row in df.iterrows()]
+    print(general_rows)
+
+    # player stats
+    sql_stats = f"select * from playerstats where player='{form.PlayerName.data}'"
+    df_stats = pd.read_sql_query(sql_stats, db.get_engine())
+    stat_rows = [row for index, row in df_stats.iterrows()]
+
+    return render_template('players/search.html',  
+                    PlayerName=form.PlayerName.data,
+                    general_rows=general_rows,
+                    stat_rows=stat_rows,
+                    tables=True, form=form)
+
+@players_bp.route('/players/add')
+def add():
+    form = PlayerAddForm()
+    return render_template('players/add.html', form=form)
+
+@players_bp.route('/players/add/create', methods=['GET', 'POST'])
+def addcreate():
+    form = PlayerAddForm()
+
+    ## stats
+    fgp = round(form.fg.data / form.fga.data, 4)
+    threepp = round(form.threep.data / form.threepa.data, 4)
+    twopp = round(form.twop.data / form.twopa.data, 4)
+    ftp = round(form.ft.data / form.fta.data, 4)
+    trb = form.orb.data + form.drb.data
+
+    sql_team = f"select teamabbr from teams where teamname='{form.team.data}'"
+    
+    df_team = pd.read_sql_query(sql_team, db.get_engine())
+
+    rows = [row for index, row in df_team.iterrows()]
+    tm = rows[0][0]
+
+    sql_stats = f"""
+    INSERT INTO playerstats(player, tm, gms, gstart, mp, fg, fga, fgp, threep, threepa, threepp, 
+    twop, twopa, twopp, efgp, ft, fta, ftp, orb, drb, trb, ast, stl, blk, tov, pf, pts)
+    VALUES ('{form.PlayerName.data}', '{tm}', {form.gms.data}, 0, 0, {form.fg.data},
+    {form.fga.data}, {fgp}, {form.threep.data}, {form.threepa.data}, {threepp}, {form.twop.data}, 
+    {form.twopa.data}, {twopp}, 0, {form.ft.data}, {form.fta.data}, {ftp}, {form.orb.data}, 
+    {form.drb.data}, {trb}, {form.ast.data}, {form.stl.data}, {form.blk.data}, {form.tov.data},
+    {form.pf.data}, {form.pts.data});
+    """
+
+    db.engine.execute(sql_stats)
+    db.session.commit()
+
+    stats = [[form.PlayerName.data], [form.team.data], [form.gms.data], [0], [0], [form.fg.data],
+    [form.fga.data], [fgp], [form.threep.data], [form.threepa.data], [threepp], [form.twop.data], 
+    [form.twopa.data], [twopp], [0], [form.ft.data], [form.fta.data], [ftp], [form.orb.data], 
+    [form.drb.data], [trb], [form.ast.data], [form.stl.data], [form.blk.data], [form.tov.data],
+    [form.pf.data], [form.pts.data]]
+
+    cols = ['player', 'tm', 'gms', 'gstart', 'mp', 'fg', 'fga', 'fgp', 'threep', 'threepa', 'threepp', 
+    'twop', 'twopa', 'twopp', 'efgp', 'ft', 'fta', 'ftp', 'orb', 'drb', 'trb', 'ast', 'stl', 'blk', 'tov', 'pf', 'pts']
+
+    d = pd.DataFrame(dict(zip(cols, stats)))
+
+    stat_rows = [row for index, row in d.iterrows()]
+
+
+    ## player
+    sql_general = f"""
+    INSERT INTO players(name, pos, age)
+    VALUES ('{form.PlayerName.data}', '{form.pos.data}', {form.age.data});
+    """
+
+    db.engine.execute(sql_general)
+    db.session.commit()
+
+
+    df_gen = pd.DataFrame(dict(name=[form.PlayerName.data], pos=[form.pos.data], age=[form.age.data]))
+    general_rows = [row for index, row in df_gen.iterrows()]
+
+    return render_template('players/search.html',  
+                    message=f"Successfully added {form.PlayerName.data}!",
+                    PlayerName=form.PlayerName.data,
+                    general_rows=general_rows,
+                    stat_rows=stat_rows,
+                    tables=True, form=form)
--- app/forms.py
+++ app/forms.py
@@ -1,8 +1,50 @@
+from flask.scaffold import F
 from flask_wtf import FlaskForm
 from wtforms.fields import StringField
 from wtforms.fields.html5 import DecimalField
+from wtforms.validators import DataRequired
 
 from wtforms.validators import Length
+from app.models import Teams
+from app.extensions import db
 
 class TeamForm(FlaskForm):
-    TeamName = StringField('Team Name', [Length(min=2, max=60)])
+    TeamName = StringField('Team Name', validators=[Length(min=2, max=60), DataRequired()])
+
+class PlayerLookupForm(FlaskForm):
+    PlayerName = StringField('Player Name', validators=[Length(min=2, max=60), DataRequired()])
+
+class PlayerAddForm(FlaskForm):
+    PlayerName = StringField('Player Name', validators=[DataRequired(), Length(min=2, max=60)])
+    team = StringField('Team', validators=[DataRequired(), Length(min=2, max=60)])
+    pos = StringField('Position', validators=[DataRequired(), Length(min=1, max=60)])
+    gms = DecimalField('Number of Games Played', validators=[DataRequired()])
+    fga = DecimalField('FGA', validators=[DataRequired()])
+    fg = DecimalField('FGM', validators=[DataRequired()])
+    twopa = DecimalField('2PA', validators=[DataRequired()])
+    twop = DecimalField('2PM', validators=[DataRequired()])
+    threepa = DecimalField('3PA', validators=[DataRequired()])
+    threep = DecimalField('3PM', validators=[DataRequired()])
+    fta = DecimalField('FTA', validators=[DataRequired()])
+    ft = DecimalField('FGM', validators=[DataRequired()])
+    orb = DecimalField('Offensive Rebounds', validators=[DataRequired()])
+    drb = DecimalField('Deffensive Rebounds', validators=[DataRequired()])
+    ast = DecimalField('Assists', validators=[DataRequired()])
+    blk = DecimalField('Blocks', validators=[DataRequired()])
+    tov = DecimalField('Turnovers', validators=[DataRequired()])
+    stl = DecimalField('Turnovers', validators=[DataRequired()])
+    pf = DecimalField('Personal Fouls', validators=[DataRequired()])
+    pts = DecimalField('Points', validators=[DataRequired()])
+    age = DecimalField('Age', validators=[DataRequired()])
+
+    # def validate(self):
+    #     check_validate = super(PlayerAddForm, self).validate()
+    #     if not check_validate:
+    #         return False
+
+    #     q = db.session.query(Teams.teamname.distinct().label('name'))
+    #     team_names = [str(row.name) for row in q.all()]
+
+    #     if str(self.team.data) not in team_names:
+    #         return False
+    #     return True
